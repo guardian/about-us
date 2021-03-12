b477c35e19f253610f612e724e28abe4cb306e99
Richard Bangay <richard.bangay@gmail.com>
2021-03-12 19:49:59 +0000

    [RB] - init commit
    Setup basic project scafold in nextjs with typescript support

---
+++ out/_next/static/CxRP4Sky7B_hBbvnfsb6l/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fchunks\u002Fpages\u002Findex-1cfb205fd6fca71a704c.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-22c6af59eeddbc517747.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
