273a9a0819b455e8cbcabc5659938462f925e258
Maria <m.olanipekun@hotmail.com>
2021-04-13 13:38:10 +0100

    create header quote component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,41 +2,25 @@
 /** @jsx jsx */
 import { jsx, css } from "@emotion/react";
 import React from "react";
-import { background } from "@guardian/src-foundations/palette";
+import { background } from "@guardian/src-foundations";
 import { GLogo } from "../components/gLogo";
+import HeaderQuote from "../components/headerQuote";
 import { PageStyles } from "../components/pageStyles";
-import Header from "../components/header";
 
 const HomePage = () => {
   const headerCss = css`
     background-color: ${background.ctaPrimary};
   `;
+
   return (
     <>
       <PageStyles />
-      <Header
-        navSections={[
-          {
-            title: "About us",
-            isSelected: true,
-            link: "https://theguardian.com",
-          },
-          {
-            title: "Our organisation",
-            isSelected: false,
-            link: "https://theguardian.com",
-          },
-          {
-            title: "Our history",
-            isSelected: false,
-            link: "https://theguardian.com",
-          },
-          {
-            title: "Our history",
-            isSelected: false,
-            link: "https://theguardian.com",
-          },
-        ]}
+      <header css={headerCss}>
+        <GLogo width={295} />
+      </header>
+      <HeaderQuote
+        quote="Since 1821 the mission of The Guardian has been to use clarity and imagination to build hope."
+        author="Katharine Viner, editor-in-chief"
       />
     </>
   );
