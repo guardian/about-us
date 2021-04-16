69eb62e095ac2d91d9c9c9e4190e9ee2becf24c0
Richard Bangay <richard.bangay@gmail.com>
2021-04-16 09:32:24 +0100

    [RB] - replace old header instantiation

--- src/pages/our-organisation/index.tsx
+++ src/pages/our-organisation/index.tsx
@@ -1,23 +1,38 @@
 /** @jsxRuntime classic /
 /** @jsx jsx */
-import { jsx, css } from "@emotion/react";
+import { jsx } from "@emotion/react";
 import React from "react";
-import { background } from "@guardian/src-foundations/palette";
-import { GLogo } from "../../components/gLogo";
+import Header from "../../components/header";
 import { PageStyles } from "../../components/pageStyles";
 
-const HomePage = () => {
-  const headerCss = css`
-    background-color: ${background.ctaPrimary};
-  `;
-  return (
-    <>
-      <PageStyles />
-      <header css={headerCss}>
-        <GLogo width={295} />
-      </header>
-    </>
-  );
-};
+const HomePage = () => (
+  <>
+    <PageStyles />
+    <Header
+      navSections={[
+        {
+          title: "About us",
+          isSelected: false,
+          link: "/",
+        },
+        {
+          title: "Our organisation",
+          isSelected: true,
+          link: "/our-organisation",
+        },
+        {
+          title: "Our history",
+          isSelected: false,
+          link: "/our-history",
+        },
+        {
+          title: "Journalism",
+          isSelected: false,
+          link: "/journalism",
+        },
+      ]}
+    />
+  </>
+);
 
 export default HomePage;
