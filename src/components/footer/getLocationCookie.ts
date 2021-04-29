954ac341ad4e802e9e1c0e59f96ad5c7155ab46b
Richard Bangay <richard.bangay@gmail.com>
2021-04-29 14:46:07 +0100

    [RB] - remove erronous = in string replace

--- src/components/footer/getLocationCookie.ts
+++ src/components/footer/getLocationCookie.ts
@@ -11,5 +11,5 @@ const getCookie = (name: string): string | null => {
   const cookies: string[] = document?.cookie
     .split(";")
     .filter((keyValue) => keyValue.trim().startsWith(`${name}=`));
-  return cookies.length ? cookies[0].trim().replace(`${name}=`, "=") : null;
+  return cookies.length ? cookies[0].trim().replace(`${name}=`, "") : null;
 };
