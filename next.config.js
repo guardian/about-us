5949c73ab43342861dfb45403d1eb03526cce86e
Richard Bangay <richard.bangay@gmail.com>
2021-04-30 09:27:20 +0100

    [RB] - add assets Prefix config for main (PROD) branch deployment and any other branch (CODE)

--- next.config.js
+++ next.config.js
@@ -1,3 +1,14 @@
-const withTM = require('next-transpile-modules')(['@guardian/consent-management-platform']); // pass the modules you would like to see transpiled
+let assetPrefix = "";
+if (process.env.TEAMCITY_BRANCH === "main") {
+  assetPrefix =
+    "https://gu-about-us.s3-eu-west-1.amazonaws.com/PROD/gu-about-us-upload";
+} else if (process.env.TEAMCITY_BRANCH.length) {
+  assetPrefix =
+    "https://gu-about-us.s3-eu-west-1.amazonaws.com/CODE/gu-about-us-upload";
+}
 
-module.exports = withTM();
+const withTM = require("next-transpile-modules")([
+  "@guardian/consent-management-platform",
+]); // pass the modules you would like to see transpiled
+
+module.exports = withTM({ assetPrefix });
