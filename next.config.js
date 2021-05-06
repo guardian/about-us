ac67ef5d58776d11d76f4c4f5bff292c1f1ab8da
Maria <m.olanipekun@hotmail.com>
2021-05-06 10:31:18 +0100

    adding a base path for the code environment so images load correctly

--- next.config.js
+++ next.config.js
@@ -1,8 +1,12 @@
 let assetPrefix = "";
+let basePath = "/about";
+
 if (process.env.TEAMCITY_BRANCH === "main") {
   assetPrefix = "/about";
 } else if (process.env.TEAMCITY_BRANCH) {
   assetPrefix =
+    "https://gu-about-us.s3-eu-west-1.amazonaws.com/CODE/gu-about-us-upload/about";
+  basePath =
     "https://gu-about-us.s3-eu-west-1.amazonaws.com/CODE/gu-about-us-upload/about";
 }
 
