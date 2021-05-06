c34ca75da3602bdb6f280556851a9bdc07265023
Maria <m.olanipekun@hotmail.com>
2021-05-06 12:17:09 +0100

    change next config base path to a relative path

--- next.config.js
+++ next.config.js
@@ -6,8 +6,7 @@ if (process.env.TEAMCITY_BRANCH === "main") {
 } else if (process.env.TEAMCITY_BRANCH) {
   assetPrefix =
     "https://gu-about-us.s3-eu-west-1.amazonaws.com/CODE/gu-about-us-upload/about";
-  basePath =
-    "https://gu-about-us.s3-eu-west-1.amazonaws.com/CODE/gu-about-us-upload/about";
+  basePath = "/CODE/gu-about-us-upload/about";
 }
 
 const withTM = require("next-transpile-modules")([
