9b8c3b82ebe31c3ca371906d6d139351e4708524
Maria Olanipekun <m.olanipekun@hotmail.com>
2022-12-19 17:37:38 +0100

    add country group helper, css styling updates

--- src/components/reader-funded/readerFundedContributeCard.tsx
+++ src/components/reader-funded/readerFundedContributeCard.tsx
@@ -2,10 +2,12 @@
 /** @jsx jsx */
 import { css, jsx, ThemeProvider } from "@emotion/react";
 import { buttonReaderRevenue, LinkButton } from "@guardian/src-button";
-import { brandAlt, neutral, space } from "@guardian/src-foundations";
+import { neutral, space } from "@guardian/src-foundations";
+import { from } from "@guardian/src-foundations/mq";
 import { body, headline } from "@guardian/src-foundations/typography";
 import { SvgArrowRightStraight } from "@guardian/src-icons";
-import { minWidth } from "../../styles/breakpoints";
+import { useEffect, useState } from "react";
+import { getCountryGroup } from "../../helpers/countryGroup";
 
 const aCss = css`
   text-decoration: none;
@@ -17,96 +19,100 @@ const h3Css = css`
   font-size: 24px;
   margin: 0;
 
-  ${minWidth.tablet} {
+  ${from.tablet} {
     font-size: 24px;
   }
 `;
 
 const pCss = css`
   color: ${neutral[100]};
-  ${body.small({ lineHeight: "loose" })}
+  ${headline.xxxsmall({ fontWeight: "regular" })};
+  margin: ${space[1]}px 0 ${space[6]}px;
+
+  ${from.tablet} {
+    ${headline.xxsmall({ fontWeight: "regular" })};
+  }
 `;
 
 const cardImageDesktopCss = css`
   display: none;
-  ${minWidth.tablet} {
+  ${from.tablet} {
     display: block;
-    height: 80%;
-    width: 50%;
-    margin: auto -12px auto auto;
-    padding-top: 20%;
+    height: 100%;
+    width: 60%;
+    margin: -20px 0 -80px;
+    padding-top: 35%;
     background-size: contain;
-    background-position: right center;
-    background-image: url("/about/images/front-page-10-desktop.png");
+    background-position: center center;
+    background-image: url("/about/images/support-the-guardian-desktop.png");
     background-repeat: no-repeat;
   }
-  ${minWidth.desktop} {
-    transform: translateY(-44px);
+  ${from.desktop} {
+    margin: -50px 0 -50px;
+    width: 50%;
+    /* margin: -20px 0 -80px; */
+    padding-top: 30%;
   }
 `;
 
 const cardImageMobileCss = css`
   width: 100%;
   padding-top: 50%;
-  background-image: url("/about/images/front-page-10-mobile.png");
+  background-image: url("/about/images/support-the-guardian-desktop.png");
   height: 100%;
   margin: 11px -${space[3]}px auto auto;
   background-size: contain;
   background-position: right top;
   background-repeat: no-repeat;
 
-  ${minWidth.tablet} {
+  ${from.tablet} {
     display: none;
   }
 `;
 
-const innerContainerCss = css`
+const desktopContainerCss = css`
   display: flex;
   flex-direction: column;
   padding-right: ${space[3]}px;
-  ${minWidth.tablet} {
+  ${from.tablet} {
     padding-right: 0;
     flex-direction: row;
     width: 100%;
   }
 `;
 
-const linkbuttonDesktop = css`
-  display: none;
-
-  ${minWidth.tablet} {
-    display: inline-flex;
-    width: max-content;
-  }
-`;
-
-const linkbuttonMobile = css`
-  display: inline-flex;
-  ${minWidth.tablet} {
-    display: none;
-  }
-`;
-
-const outerContainerCss = css`
+const mobileContainerCss = css`
   display: flex;
   border-top: 1px solid #90abc4;
-  ${minWidth.tablet} {
+  ${from.tablet} {
     display: block;
     border-top: none;
   }
 `;
 
 const ReaderFundedContributeCard = () => {
+  const [currencyGlyph, setCurrencyGlyph] = useState("£");
+
+  useEffect(() => {
+    const currencyGlyph = getCountryGroup()?.currencyGlyph;
+
+    currencyGlyph && setCurrencyGlyph(currencyGlyph);
+  }, []);
+
   return (
-    <div css={outerContainerCss}>
-      <div css={innerContainerCss}>
-        <div>
+    <div css={mobileContainerCss}>
+      <div css={desktopContainerCss}>
+        <div
+          css={css`
+            max-width: 510px;
+          `}
+        >
           <a css={aCss} href="https://support.theguardian.com/contribute">
             <h3 css={h3Css}>Show your support as often as you like</h3>
           </a>
           <p css={pCss}>
-            Choose to give once from $1, £1 or €1, or set up a recurring
-            contribution to support us each month or year.
+            Choose to give once from just {currencyGlyph}1, or set up a
+            recurring amount to power our reporting every month or year.
           </p>
           <ThemeProvider theme={buttonReaderRevenue}>
             <LinkButton
@@ -115,24 +121,11 @@ const ReaderFundedContributeCard = () => {
               iconSide="right"
               nudgeIcon={true}
               href="https://support.theguardian.com/contribute"
-              css={linkbuttonDesktop}
             >
-              Contribute
+              Support us
             </LinkButton>
           </ThemeProvider>
         </div>
-        <ThemeProvider theme={buttonReaderRevenue}>
-          <LinkButton
-            size="small"
-            icon={<SvgArrowRightStraight />}
-            iconSide="right"
-            nudgeIcon={true}
-            href="https://support.theguardian.com/contribute"
-            css={linkbuttonMobile}
-          >
-            Support us
-          </LinkButton>
-        </ThemeProvider>
         <div css={cardImageDesktopCss} />
       </div>
       <div css={cardImageMobileCss} />
